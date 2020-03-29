d754291cd92dbc2be8263f50d07471817a3a5612
Berenice Medel <bemesa21@gmail.com>
2020-03-29 00:36:24 -0600

    print all comparatives

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from hash_algorithms import compare_hash_algoritms
 from signature_algorithms import compare_signature_algorithms
 from encrypt_algoritms import compare_encrypt_algoritms
 
-#compare_hash_algoritms()
-#compare_signature_algorithms()    
+compare_hash_algoritms()
+compare_signature_algorithms()    
 compare_encrypt_algoritms()
